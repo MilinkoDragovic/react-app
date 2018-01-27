cb6f7b5906c49a93e8c6db6abe247e9f26536814
m.dragovic@kompitenz.de <m.dragovic@kompitenz.de>
2018-01-27 13:47:52 +0100

    components and props

--- src/index.js
+++ src/index.js
@@ -2,10 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './style.css'
 
-setInterval(function () {
-    ReactDOM.render(
-        <h2>Current time: {new Date().toLocaleTimeString()}</h2>,
-        document.getElementById('app')
-    );
-}, 1000);
+
+function Cartoon(props) {
+        return <h1>Zdravo, {props.name} {props.show}</h1>
+}
+
+function Show (){
+    return <div>
+        <Cartoon name="Vladjimir" show="Bleso"/>
+        <Cartoon name="Mili" show="Kralj"/>
+    </div>
+}
+
+// class Cartoon extends React.Component {
+//     render(){
+//         return <h1>Hello {this.props.name}</h1>
+//     }
+// }
+
+ReactDOM.render(
+    <Show />,
+    document.getElementById('app')
+);
+
 
